1c58169dd536b08ab78e5e215cc396a41d94b7a1
Lakshya Kapoor <kapoorlakshya@gmail.com>
2018-10-10 22:43:42 -0700

    Add more specs for FFMPEG::Screenrecorder.

--- spec/ffmpeg/screenrecorder_spec.rb
+++ spec/ffmpeg/screenrecorder_spec.rb
@@ -1,4 +1,9 @@
 RSpec.describe FFMPEG::Screenrecorder do
+  before do
+    @opts     = { output: 'C:\test-output.mkv' }
+    @recorder = FFMPEG::Screenrecorder.new(@opts)
+  end
+
   it 'has a version number' do
     expect(FFMPEG::Screenrecorder::VERSION).not_to be nil
   end
@@ -6,4 +11,23 @@ RSpec.describe FFMPEG::Screenrecorder do
   it 'it can find the FFMPEG binary' do
     expect(`#{FFMPEG.ffmpeg_binary} -version`).to include('ffmpeg version')
   end
+
+  it 'returns an IO object when #start is invoked' do
+
+  end
+
+  it 'returns 1 when #stop is invoked' do
+
+  end
+
+  it 'can record the desktop and output a file' do
+    @recorder.start
+    @recorder.stop
+    expect(File).to exist(@opts[:output])
+  end
+
+  it 'can record at a given FPS of 15' do
+
+  end
+
 end
