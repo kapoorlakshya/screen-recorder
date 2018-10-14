770783a8579709bee4b0b76ae392ca4d725b364a
Lakshya Kapoor <kapoorlakshya@gmail.com>
2018-10-14 16:16:40 -0700

    Screenrecorder - Implement basic start-stop feature.

--- lib/ffmpeg.rb
+++ lib/ffmpeg.rb
@@ -1,3 +1,5 @@
+require 'ffmpeg/screenrecorder'
+
 module FFMPEG
   module Screenrecorder
   end
--- lib/ffmpeg/screenrecorder.rb
+++ lib/ffmpeg/screenrecorder.rb
@@ -1,8 +1,42 @@
 require 'streamio-ffmpeg'
-require 'pry-byebug'
+require 'os'
 
 module FFMPEG
-  module Screenrecorder
-    # Your code goes here...
+  class Recorder
+    attr_reader :opts, :ffmpeg, :output
+    attr_accessor :process_id
+
+    def initialize(opts)
+      @opts       = default_config.merge opts
+      @ffmpeg     = FFMPEG.ffmpeg_binary
+      @output     = opts[:output]
+      @process_id = nil
+    end
+
+    def start
+      @process_id = spawn(command)
+    end
+
+    def stop
+      # return Process.kill('EXIT', @process_id) if OS.linux?
+      `TASKKILL /f /pid ffmpeg.exe`
+      # Process.detach(@process_id)
+    end
+
+    def inputs
+      %w(desktop)
+    end
+
+    private
+
+    def default_config
+      { input: 'desktop', framerate: 15, device: 'gdigrab' }
+    end
+
+    def command
+      # "ffmpeg -f gdigrab -framerate 15 -i desktop output.mkv 2> log.txt"
+      "#{ffmpeg} -y -f #{opts[:device]} -framerate #{opts[:framerate]} -i #{opts[:input]} #{opts[:output]} 2> log.txt"
+    end
+
   end
 end
