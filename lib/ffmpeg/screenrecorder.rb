ebce8f90bf3613aff271f7f399964f1a583036fb
Lakshya Kapoor <kapoorlakshya@gmail.com>
2018-10-14 19:41:15 -0700

    Revert "Recorder#command - Use HEREDOC for multi-line String."
    This reverts commit be47d83e89eb3617551f347a4d28619b4872c4fb.

--- lib/ffmpeg/screenrecorder.rb
+++ lib/ffmpeg/screenrecorder.rb
@@ -37,14 +37,6 @@ module FFMPEG
     end
 
     def command
-      <<-EOS
-      #{ffmpeg} -y
-      -f #{opts[:device]}
-      -framerate #{opts[:framerate]}
-       -i #{opts[:input]}
-       #{opts[:output]}
-       2> #{@opts[:log]}"
-      EOS
       # "ffmpeg -f gdigrab -framerate 15 -i desktop output.mkv 2> log.txt"
       "#{ffmpeg} -y -f #{opts[:device]} -framerate #{opts[:framerate]} -i #{opts[:input]} #{opts[:output]} 2> #{@opts[:log]}"
     end
