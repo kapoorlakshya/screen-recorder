98a5a2314526fa40bc548d21969084e621c6267c
Lakshya Kapoor <kapoorlakshya@gmail.com>
2018-10-17 19:29:45 -0700

    gemspec + spec - Add watir and webdrivers gem for browser based tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'bundler/setup'
 require 'ffmpeg/screenrecorder'
+require 'watir'
+require 'webdrivers'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
