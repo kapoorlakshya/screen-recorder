1ae4c8d7ab5938d3b1c33a14ed3c7b66c1920681
Lakshya Kapoor <kapoorlakshya@gmail.com>
2018-10-18 00:08:26 -0700

    Break down spec based on each method behavior and finish spec for #inputs.

--- spec/ffmpeg/screenrecorder_spec.rb
+++ spec/ffmpeg/screenrecorder_spec.rb
@@ -13,84 +13,110 @@ RSpec.describe FFMPEG::Screenrecorder do
 end
 
 RSpec.describe FFMPEG::Recorder do
-  context 'given the recorder has been initialized' do
+  before(:all) do
+    opts      = { output:     'ffmpeg-screenrecorder-rspec-output.mkv',
+                  input:      'desktop',
+                  framerate:  30.0,
+                  extra_opts: { video_size: '1024x768' } }
+    @recorder = FFMPEG::Recorder.new(opts)
+    @browser  = nil
+  end
+
+  context 'given FFMPEG::Recorder has been initialized' do
+    describe '#opts' do
+      it 'returns a Hash of options' do
+        expect(@recorder.opts).to be_a(Hash)
+      end
+    end
+
+    describe '#start' do
+      it 'returns pid' do
+        pid = @recorder.start
+        expect(pid).to be_a_kind_of(Integer)
+      end
+
+      it 'creates a log file' do
+        sleep(1.0) # Wait for file generation
+        expect(File).to exist(@recorder.opts[:log])
+      end
+    end
+
+    describe '#stop' do
+      it 'returns a SUCCESS message' do
+        duration = 10.0
+        puts "\tWaiting #{duration}s for recording to complete..."
+        sleep(duration) # Takes 10s to create a valid recording
+        expect(@recorder.stop).to include('SUCCESS')
+      end
+
+      it 'creates a output file' do
+        expect(File).to exist(@recorder.output)
+      end
+
+      it 'creates a valid video file' do
+        expect(@recorder.video_file.valid?).to be(true)
+      end
+    end
+  end
+
+  context 'given FFMPEG::Recorder accepts user defined parameters' do
+    describe '#opts[:extra_opts]' do
+      it 'records at the given FPS' do
+        expect(@recorder.video_file.frame_rate).to equal(@recorder.opts[:framerate])
+      end
+
+      it 'records at user given resolution' do
+        expect(@recorder.video_file.resolution).to eq(@recorder.opts[:extra_opts][:video_size])
+      end
+    end
+  end
+
+  context 'given a firefox window is open' do
     before(:all) do
-      opts      = { output:     'ffmpeg-screenrecorder-rspec-output.mkv',
-                    input:      'desktop',
-                    framerate:  30.0,
-                    extra_opts: { video_size: '1024x768' } }
-      @recorder = FFMPEG::Recorder.new(opts)
+      # Facing a weird issue where the .webdrivers folder is not found
+      # after the first time geckodriver is downloaded,
+      # @todo Troubleshoot and remove this temporary fix.
+      FileUtils.rm_rf('C:\Users\Lakshya Kapoor\.webdrivers')
+
+      @browser = Watir::Browser.new :firefox
+      @browser.goto 'google.com'
     end
 
-    it 'returns a Hash for #opts' do
-      expect(@recorder.opts).to be_a(Hash)
+    describe '#inputs' do
+      it 'return a list of available browser windows as inputs (recording regions)' do
+        expect(@recorder.inputs('firefox')).to be_a_kind_of(Array)
+      end
+
+      it 'returns the title of the currently open browser window' do
+        expect(@recorder.inputs('firefox').first).to eql('Window Title: Google - Mozilla Firefox')
+      end
     end
 
-    it 'returns pid on #start' do
-      pid = @recorder.start
-      expect(pid).to be_a_kind_of(Integer)
-    end
-
-    it 'creates a log file on #start' do
-      sleep(1.0) # Wait for file generation
-      expect(File).to exist(@recorder.opts[:log])
-    end
-
-    it 'returns a SUCCESS message on #stop ' do
-      duration = 10.0
-      puts "\tWaiting #{duration}s for recording to complete..."
-      sleep(duration) # Takes 10s to create a valid recording
-      expect(@recorder.stop).to include('SUCCESS')
-    end
-
-    it 'creates a output file when #stop is invoked' do
-      expect(File).to exist(@recorder.output)
-    end
-
-    it 'creates a valid video file when #stop is invoked' do
-      expect(@recorder.video_file.valid?).to be(true)
-    end
-
-    it 'can record at a user given FPS' do
-      expect(@recorder.video_file.frame_rate).to equal(@recorder.opts[:framerate])
-    end
-
-    it 'uses recording resolution from extra_opts' do
-      expect(@recorder.video_file.resolution).to eq(@recorder.opts[:extra_opts][:video_size])
-    end
-
-    it 'can return a list of available inputs (recording regions)' do
-      app     = :firefox
-      browser = Watir::Browser.new app
-      browser.goto 'google.com'
-      expect(@recorder.inputs(app)).to be_a_kind_of(Array)
-      browser.quit
-    end
-
-    # it 'can record a browser window' do
-    #   browser = Watir::Browser.new :firefox
-    #   browser.resize_to(800, 600)
-    #   browser.goto 'google.com'
-    #   inputs = FFMPEG::Recorder.inputs # Tab with title as Google
-    #
-    #   opts     = { output:    'C:\ffmpeg-screenrecorder-rspec-output.mkv',
-    #                input:     inputs.first,
-    #                framerate: 15 }
-    #   recorder = FFMPEG::Screenrecorder.new(opts)
-    #   recorder.start
-    #   3.times { browser.refresh }
-    #   recorder.stop
-    #
-    #   # @todo Can't think of a valid test for this...
-    # end
-
-    #
-    # Clean up
-    #
     after(:all) do
-      `rm #{@recorder.output}`
-      sleep(0.5)
-      `rm #{@recorder.opts[:log]}`
+      @browser.quit
     end
+  end
+
+# it 'can record a browser window' do
+#   browser = Watir::Browser.new :firefox
+#   browser.resize_to(800, 600)
+#   browser.goto 'google.com'
+#   inputs = FFMPEG::Recorder.inputs # Tab with title as Google
+#
+#   opts     = { output:    'C:\ffmpeg-screenrecorder-rspec-output.mkv',
+#                input:     inputs.first,
+#                framerate: 15 }
+#   recorder = FFMPEG::Screenrecorder.new(opts)
+#   recorder.start
+#   3.times { browser.refresh }
+#   recorder.stop
+#
+#   # @todo Can't think of a valid test for this...
+# end
+#
+  after(:all) do
+    FileUtils.rm @recorder.output
+    sleep(0.5)
+    FileUtils.rm @recorder.opts[:log]
   end
 end
