27148e2bc8d085c56fe0396f1378f6c83939b698
Lakshya Kapoor <kapoorlakshya@gmail.com>
2018-10-18 00:08:09 -0700

    spec_helper - Require pry-byebug and fileutils.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'bundler/setup'
 require 'ffmpeg/screenrecorder'
 require 'watir'
 require 'webdrivers'
+require 'pry-byebug'
+require 'fileutils'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
