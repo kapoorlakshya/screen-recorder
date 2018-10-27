c9389c6e333e03cfc69d81b8cd1e742c35bba775
kapoorlakshya <kapoorlakshya@gmail.com>
2018-10-27 16:45:46 -0700

    Add RecorderOptions to handle ffmpeg parameters.

---
+++ lib/ffmpeg/recorder_options.rb
@@ -0,0 +1,67 @@
+module FFMPEG
+  class RecorderOptions
+
+    def initialize(options)
+      @options = verify_options options
+    end
+
+    def values
+      @options
+    end
+
+    def parsed_values
+      vals = "-fmt #{@options[:format]} "
+      vals << "-framerate #{@options[:framerate]} "
+      vals << advanced_options if @options[:advanced]
+      vals << "-infile #{@options[:infile]} "
+      vals << "#{@options[:output]}"
+      vals << ffmpeg_log_to(@options[:log]) # If provided
+    end
+
+    private
+
+    #
+    # Verifies the required options are provided
+    #
+    def verify_options(options)
+      missing_options = required_options.collect { |req| options[req].nil? }
+      raise "Required options are missing: #{missing_options}" unless missing_options.empty?
+
+      options
+    end
+
+    #
+    # Returns Array of require options a Symbols
+    #
+    def required_options
+      # -f format
+      # -r framerate
+      # -i input
+      # output
+      %i[format framerate infile output]
+    end
+
+    #
+    # Returns advanced options parsed and ready for ffmpeg to receive.
+    #
+    def advanced_options
+      return nil unless @options[:advanced]
+      raise ':advanced cannot be empty.' if options[:advanced].empty?
+
+      arr = []
+      options[:advanced].each { |k, v|
+        arr.push "-#{k} #{v}"
+      }
+      arr.join(' ') + ' '
+    end
+
+    #
+    # Determines if the ffmpeg output will be to a log
+    # file based on given options.
+    #
+    def ffmpeg_log_to(file)
+      return " 2> #{file}" if file
+      ' > nul 2>&1' # No log file given
+    end
+  end
+end
