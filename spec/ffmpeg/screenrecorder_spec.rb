60295b9f7b580eef7b0e17dde6435aace456f122
Lakshya Kapoor <kapoorlakshya@gmail.com>
2018-11-01 15:38:23 -0700

    Spec - Add back test for gem version number.

--- spec/ffmpeg/screenrecorder_spec.rb
+++ spec/ffmpeg/screenrecorder_spec.rb
@@ -2,6 +2,10 @@ require_relative '../spec_helper'
 
 RSpec.describe FFMPEG::Screenrecorder do
   context 'given the gem is loaded' do
+    it 'has a version number' do
+      expect(FFMPEG::Screenrecorder::VERSION).not_to be nil
+    end
+
     it 'it can find the FFmpeg binary' do
       expect(`#{FFMPEG.ffmpeg_binary} -version`).to include('ffmpeg version')
     end
