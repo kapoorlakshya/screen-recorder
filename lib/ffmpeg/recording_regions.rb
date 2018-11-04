f44ee460b71b8140def7484b2deaf104fa8b05b6
Lakshya Kapoor <kapoorlakshya@gmail.com>
2018-11-04 01:49:14 -0700

    RecordingRegios - Add support for linux.

--- lib/ffmpeg/recording_regions.rb
+++ lib/ffmpeg/recording_regions.rb
@@ -18,17 +18,37 @@ module FFMPEG
       # Note: Only supports Windows OS as of version beta2.
       #
       def available_windows_for(application)
-        raw_list   = `tasklist /v /fi "imagename eq #{application}.exe" /fo list | findstr  Window`
-                       .split("\n")
-                       .reject { |title| title == 'Window Title: N/A' }
-        final_list = raw_list.map { |i| i.gsub('Window Title: ', '') } # Make it user friendly
+        windows_os(application) if OS.windows?
+        linux_os(application) if OS.linux?
+      end
 
-        raise ApplicationNotFound, "No open windows found for: #{application}.exe" if final_list.empty?
+      private
+
+      def windows_os(application)
+        raw_list   = `tasklist /v /fi "imagename eq #{application}.exe" /fo list | findstr  Window`
+                     .split("\n")
+                     .reject { |title| title == 'Window Title: N/A' }
+        final_list = raw_list.map { |i| i.gsub('Window Title: ', '') } # Make it user friendly
+        raise RecorderErrors::ApplicationNotFound, "No open windows found for: #{application}.exe" if final_list.empty?
+
         final_list
       end
-    end
 
-    # @since 1.0.0-beta3
-    class ApplicationNotFound < StandardError; end
+      def linux_os(application)
+        raise DependencyNotFound, 'wmctrl is not installed. Run: sudo apt-get install wmctrl.' unless wmctrl_installed?
+
+        final_list = `wmctrl -l | awk '{$3=""; $2=""; $1=""; print $0}'` # Returns all open windows
+                     .split("\n")
+                     .map(&:strip)
+                     .select { |t| t.match?(/#{application}/i) } # Narrow down to given application
+        raise RecorderErrors::ApplicationNotFound, "No open windows found for: #{application}" if final_list.empty?
+
+        final_list
+      end
+
+      def wmctrl_installed?
+        !`which wmctrl`.empty? # "" when not found
+      end
+    end
   end # module Windows
 end # module FFMPEG
