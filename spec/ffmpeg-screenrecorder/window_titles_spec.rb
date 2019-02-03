1cf509525f642744c9a01a2444f0cea3ce9b6f4a
Lakshya Kapoor <kapoorlakshya@gmail.com>
2019-02-03 12:08:14 -0800

    Rename spec/ffmpeg folder to have proper gem name.

---
+++ spec/ffmpeg-screenrecorder/window_titles_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../spec_helper'
+
+#
+# Windows Only
+#
+if OS.windows? # Only gdigrab supports window capture
+  RSpec.describe FFMPEG::WindowTitles do
+    describe '#fetch' do
+      let(:browser_process) {
+        'firefox'
+      }
+
+      let(:browser) do
+        Webdrivers.install_dir = 'webdrivers_bin'
+        Watir::Browser.new :firefox
+      end
+
+      let(:url) {
+        'https://google.com'
+      }
+
+      let(:expected_title) {
+        ['Google - Mozilla Firefox']
+      }
+
+      it 'returns window title from Mozilla Firefox' do
+        # Note: browser is lazily loaded with let
+        browser.goto url
+        browser.wait
+        expect(FFMPEG::WindowTitles.fetch(browser_process)).to eql(expected_title)
+        browser.quit
+      end
+    end # describe
+  end # Os.windows?
+end
