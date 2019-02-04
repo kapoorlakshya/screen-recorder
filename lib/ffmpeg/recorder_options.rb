ed75fb5f27afc9d0bff8e4a7ea75f9b103b3b647
Lakshya Kapoor <kapoorlakshya@gmail.com>
2019-02-03 22:22:29 -0800

    Support user given display number (input) in Linux.

--- lib/ffmpeg/recorder_options.rb
+++ lib/ffmpeg/recorder_options.rb
@@ -135,7 +135,11 @@ module FFMPEG
     #
     def determine_input
       # x11grab doesn't support window capture
-      return ':0.0' if OS.linux?
+      if OS.linux?
+        return ':0.0' if @options[:input] == 'desktop'
+
+        return @options[:input] # User given display number
+      end
 
       return @options[:input] if @options[:input] == 'desktop'
 
