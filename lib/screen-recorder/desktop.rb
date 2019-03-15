ff59c5c3576d72b2dbb35c94dec59bb6ed35527b
kapoorlakshya <kapoorlakshya@gmail.com>
2019-03-15 01:22:30 -0700

    Fix rubocop warnings.

--- lib/screen-recorder/desktop.rb
+++ lib/screen-recorder/desktop.rb
@@ -20,6 +20,7 @@ module ScreenRecorder
     def determine_input(val)
       if OS.linux?
         return DEFAULT_INPUT_LINUX if val == 'desktop'
+
         return val # Custom $DISPLAY number in Linux
       end
       return DEFAULT_INPUT_WIN if OS.windows?
