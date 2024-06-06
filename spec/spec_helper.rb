3e87d4acdcdf89aacdb3b0c3374d1c371c13add8
Lakshya Kapoor <4314581+kapoorlakshya@users.noreply.github.com>
2024-06-06 05:11:23 +0000

    Default Linux display to :0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -51,7 +51,7 @@ end
 #
 def test_input
   if OS.linux?
-    `echo $DISPLAY`.strip || ':0' # If $DISPLAY is not set, use default of :0.0
+    ':0'
   elsif OS.mac?
     ENV['CI'] ? '0' : '1'
   elsif OS.windows?
